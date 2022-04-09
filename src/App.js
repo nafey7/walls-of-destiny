5e795cec1a6207fc27e959999c8b0952a7590b2a
mahadmubashir <mahadlgs@gmail.com>
2022-04-09 07:20:30 +0500

    adding product screen

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Block from './components/Block';
-
+import Product from './components/product';
 
 function App() {
   return (
     <div className="App" style={{height:"100%", backgroundColor:"red", position:"relative", width:"100%"}}>
-    <Navbar />
+    {/* <Navbar />
     <Block />
     
     <Footer />
-    
+     */}
+    <Product />
     </div>
   );
 }
