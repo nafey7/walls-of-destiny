f1cb36cd834bc0964a5ec28b4bb8ce74d65a10af
SilalChattha <silalanwar97@gmail.com>
2022-04-17 00:51:45 +0500

    new

--- src/components/AdminHome.js
+++ src/components/AdminHome.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import axios from 'axios';
+
 
 
 function AdminHome(props) {
-    
+    const [prod,setProd] = React.useState([{}]);
+    React.useEffect(()=> {
+      axios.get('http://localhost:8000/home')
+      .then(function(res) {
+          setProd(res.data)          
+      }, prod)
+      .catch(function(err) {
+          console.log(err);
+      })
+    }, [])
 
     return (
       <>
     <button className="btn btn-dark">Add Product</button>
 
-        <h2 style={{textAlign:"center"}}>{props.title}</h2>
+        <h2 style={{textAlign:"center"}}>All Products</h2>
                <div class="row" style={{margin:"0 0 55px 0cm"}}>
-                   {props.featured.map((item)=>{
+                   {prod.map((item)=>{
                      
                        return(
                          
@@ -19,7 +30,7 @@ function AdminHome(props) {
                            <button ><img className="card-img-top" src={item.pic} alt="Card image cap"/></button>
                            <div className="card-body" style={{backgroundColor:"grey"}}>
                              <p className="card-text"><h4><b>{item.name}</b></h4></p>
-                             <h4><b>Rs: {item.price} /-</b></h4>
+                             <h4><b>Rs: {item.sales_price} /-</b></h4>
                            </div>
                          </div>
                          </div>
