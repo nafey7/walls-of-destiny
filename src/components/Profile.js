34ef0b243e5d41fc177d796af9f1d8ed34d6d5fa
SilalChattha <silalanwar97@gmail.com>
2022-04-17 23:17:24 +0500

    new

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,75 @@
 import Avatar from '@mui/material/Avatar';
 import {Link} from 'react-router-dom'
+import React from 'react'
+import {useLocation, useNavigate} from "react-router-dom";
+import {reactLocalStorage} from 'reactjs-localstorage';
+import axios from 'axios';
+import { useFormik } from 'formik';
 
 function Profile() {
+    let username = reactLocalStorage.get('username', "", true);
+    const [cust, setCust] = React.useState({
+        address: '',
+        contact: '',
+        username: username,
+        password: '',
+        name: '',
+    })
+    // const formik = useFormik({
+    //     initialValues: {
+    //         address: '',
+    //         contact: '',
+    //         username: '',
+    //         password: '',
+    //         name: '',
+    //       },
+    //       enableReinitialize: true,
+    //       onSubmit: values =>{
+
+    //         axios.post('http://localhost:8000/update_customer_info',{
+    //             username: username,
+    //             password: values.password, 
+    //             name: values.name,
+    //             address: values.address, 
+    //             contact: values.contact
+    //         })
+    //         .then(function(res) {
+    //             alert("Change Successful!");                        
+    //         })
+    //         .catch(function(err) {
+    //             console.log(err);
+    //     })}
+    // })
+
+    function changepassword(e){
+        let x = cust;
+        x.password = e;
+        setCust(x)
+    }
+    function changeaddress(e){
+        let x = cust;
+        x.address = e;
+        setCust(x)
+    }
+    function changename(e){
+        let x = cust;
+        x.name = e;
+        setCust(x)
+    }
+    function changecontact(e){
+        let x = cust;
+        x.contact = e;
+        setCust(x)
+    }
+    function done(){
+        axios.post('http://localhost:8000/update_customer_info',cust)
+            .then(function(res) {
+                alert("Change Successful!");                        
+            })
+            .catch(function(err) {
+                console.log(err);
+        })}
+
     return (
         <div className="container" style={{ margin: "50px 150px", height:"50%", backgroundColor:"white", position:"relative", width:"100%"}}>
         <div className="row">
@@ -21,31 +89,24 @@ function Profile() {
         <div className="row">
             <div className="col">
             
-            <label for="usernameInput" className="form-label">Username</label>
-            <input className="form-control" id = "usernameInput" type="text" placeholder="Username" />
-            <br></br>
-            <label for="emailInput" className="form-label">Email</label>
-            <input className="form-control" id = "emailInput" type="email" placeholder="email@example.com" />
+            <label for="addressInput" className="form-label">House Address</label>
+            <input className="form-control" type="text" id = "addressInput" name= "addressInput" onChange={(e)=>{changeaddress(e.target.value)}}  placeholder="House#,City"/>
             <br></br>
             <label for="contactInput" className="form-label">Contact</label>
-            <input className="form-control" id = "contactInput" type="text" placeholder="+92 300 *******" />
+            <input className="form-control" id = "contactInput" name="contactInput" onChange={(e)=>{changecontact(e.target.value)}}  type="text" placeholder="+92 300 *******" />
             <br></br>
             <Link to={{pathname: "/"}} style={{backgroundColor:"",margin:"0 20px", fontSize:"18px"}}>
             <button type="button" className="btn btn-dark">Back</button></Link>
             <br></br>
             </div>
-
             <div className="col" style ={{ margin: "0 50px"}}>
             <label for="nameInput" className="form-label">Name</label>
-            <input className="form-control" type="text" id = "nameInput" placeholder="firstname lastname"/>
+            <input className="form-control" type="text" id = "nameInput" name= "nameInput" onChange={(e)=>{changename(e.target.value)}} placeholder="firstname lastname"/>
             <br></br>
             <label for="passwordInput" className="form-label">Password</label>
-            <input className="form-control" type="text" id = "passwordInput" placeholder="********"/>
+            <input className="form-control" type="text" id = "passwordInput" name= "passwordInput" oonChange={(e)=>{changepassword(e.target.value)}} placeholder="********"/>
             <br></br>
-            <label for="addressInput" className="form-label">House Address</label>
-            <input className="form-control" type="text" id = "addressInput" placeholder="House#,City"/>
-            <br></br>
-            <button type="button" className="btn btn-success">Apply</button>
+            <button type="button" onClick={done} className="btn btn-success">Apply</button>
             <br></br>
             </div>
             <div className="col-4">
